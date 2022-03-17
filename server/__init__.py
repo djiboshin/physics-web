bb3f21dc074a25ced3a585954ffd093fbf8d7832
Vladimir <v.igoshin01@mail.ru>
2022-03-17 19:57:40 +0300

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -4,10 +4,13 @@ from .routes import routes
 import aiohttp_jinja2
 import jinja2
 import logging
+import sys
 
 PATH = pathlib.Path(__file__).parent
 
+handler = logging.StreamHandler(sys.stdout)
+logging.basicConfig(level=logging.INFO, handlers=[handler])
+
 app = web.Application()
 app.add_routes(routes)
-logging.basicConfig(level=logging.INFO)
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(PATH.joinpath('templates')))
